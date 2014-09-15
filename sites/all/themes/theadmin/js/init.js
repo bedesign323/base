b17c5a35ce96bb5a9658aa1a3a1f7df514ced1d7
BE Design <info@b-ed.com>
2014-09-15 00:45:56 -0700

    work on admin nav bar

--- sites/all/themes/theadmin/js/init.js
+++ sites/all/themes/theadmin/js/init.js
@@ -1,12 +1,44 @@
-Drupal.behaviors.init = {
+Drupal.behaviors.init_admin = {
 	attach: function (context, settings) {
-		
-		var win = jQuery(window);
-		var doc = jQuery(document);
-		var win_w = win.width();
-		var win_h = win.height();
+		(function ($) {
+ 			
+ 			
+			// MENU TWEAKS!!!
+ 			//===================================
+ 			$('.menu-toggle').click(function(){
+				$('#header-container #menu').slideToggle(300);
+				return false;
+			});
+
+ 			$('#block-menu-menu-common-tasks .content > ul > li').click(function() {
+ 				$('#block-menu-menu-common-tasks .content > ul > li > ul').slideUp(300);
+ 				$('ul', this).slideDown(300);
+
+
+ 			});
 
 
 
-	}
+ 			// MASONRY!!!
+ 			//===================================
+			// var masonry_container = $('.node-image-gallery .field-name-field-other-images');
+			
+			// masonry_container.imagesLoaded(function(){
+			// 	masonry_container.masonry({
+			// 	  itemSelector: '.field-item'
+			// 	});
+			// });
+
+
+			// SKROLR!!!
+ 			//===================================
+			// if($(window).width() > 500){
+	 		//		var s = skrollr.init({
+	 		// 		forceHeight: false,
+	 		// 	});
+	 		// }
+
+
+		}(jQuery));
+	}	
 }
